4fcc77f61a5c78fcb60763c60f0875eb52033347
Iman Khoshabi <iman.neofight@gmail.com>
2021-11-18 14:00:58 +0330

    Make default theme dark

--- lib/my_theme_model.dart
+++ lib/my_theme_model.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyThemeModel extends ChangeNotifier {
-  bool _isDark = false;
+  bool _isDark = true;
 
   ThemeMode currentTheme() {
     return _isDark ? ThemeMode.dark : ThemeMode.light;
